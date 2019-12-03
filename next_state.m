1fd3c0a65ca37a21f4d0b13c85708ed56a36c357
lucasbeto <skybilobran@gmail.com>
2019-12-03 04:50:22 -0300

    Add main decoder loop

--- next_state.m
+++ next_state.m
@@ -1,10 +1,10 @@
 function [ next ] = next_state( in, state, s )
 %NEXT_STATE Summary of this function goes here
 %   Detailed explanation goes here
-    i_b = de2bi(state, s);
+    i_b = de2bi(state -1, s);
     i_b = circshift( flip(i_b) ,[1 1] );
     i_b(1) = in;
 
-    next = i_b;
+    next = bi2de( flip(i_b) ) + 1;
 end
 
--- viterbi_decoder.m
+++ viterbi_decoder.m
@@ -3,8 +3,8 @@ function [ ue ] = viterbi_decoder( v, state_table, s)
 %   Detailed explanation goes here
     
 %% matriz de custo e de posição
-    C = Inf(2^s, 10000);
-    P = zeros(2^s, 10000);
+    C = Inf(2^s, 10001);
+    P = zeros(2^s, 10001);
     
 %%
     for i = 1000
@@ -12,6 +12,19 @@ function [ ue ] = viterbi_decoder( v, state_table, s)
         slice = v((3*i-2):(3*i));
         
         for j = s
+
+            for k = 0:1
+                next_j = next_state(k, j, s);
+                
+                if C(j, i) ~= Inf %% safa as primeiras 2 colunas. Acho que nem precisa
+                    aux = C(j, i) + hamming_weight( slice, state_table( j ,1+k) );
+                    if C(next_j, i+1) > aux
+                        C(next_j, i+1) = aux;
+                        P(next_j, i+1) = j;
+                    end
+                end
+            
+            end
             
         end
         
